a0ff30f17f0849c39f065f9bbaf314896bf54f8e
Edsko de Vries <edsko@well-typed.com>
2021-11-18 15:38:47 +0100

    Add some smaller test case for experimentation

--- test/Test/Sanity.hs
+++ test/Test/Sanity.hs
@@ -1,5 +1,5 @@
 {-# OPTIONS_GHC -fplugin=TypeLet #-}
-{-# OPTIONS_GHC -fdefer-type-errors -Wwarn=deferred-type-errors #-}
+-- {-# OPTIONS_GHC -fdefer-type-errors -Wwarn=deferred-type-errors #-}
 
 module Test.Sanity (tests) where
 
@@ -12,15 +12,24 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
 
+import Test.Size.Setup
+
 tests :: TestTree
 tests = testGroup "Test.Sanity" [
-      testGroup "valid" [
+      testGroup "simple" [
           testProperty  "reflexive"   $ castIsId castReflexive
         , testProperty  "singleLet"   $ castIsId castSingleLet
         , testProperty  "singleLetAs" $ castIsId castSingleLetAs
         ]
     , testGroup "invalid" [
           testCase "notEqual" $ expectTypeError castNotEqual
+        ]
+    , testGroup "HList" [
+          testCase "LetAs"    $ testHList hlistLetAs
+        , testCase "LetAsCPS" $ testHList hlistLetAsCPS
+        ]
+    , testGroup "Ap" [
+          testCase "Let" $ testAp apLet
         ]
     ]
 
@@ -33,6 +42,19 @@ expectTypeError x = do
     case mErr of
       Left _err -> return () -- TODO: Should we be more precise?
       Right _a' -> assertFailure "Expected type error"
+
+testHList :: HList '[T "i00", T "i01", T "i02"] -> Assertion
+testHList =
+    assertEqual "" hlistBaseline
+
+testAp ::
+     (forall f r. Applicative f => (T "i00" -> T "i01" -> T "i02" -> r) -> f r)
+  -> Assertion
+testAp apTest =
+    assertEqual "" (runIdentity $ apBaseline f) (runIdentity $ apTest f)
+  where
+    f :: T "i00" -> T "i01" -> T "i02" -> HList '[T "i00", T "i01", T "i02"]
+    f x y z = HCons x $ HCons y $ HCons z $ HNil
 
 {-------------------------------------------------------------------------------
   Simple casts
@@ -47,7 +69,7 @@ castReflexive = castEqual
 
 -- | Test that we cannot cast between different types
 castNotEqual :: Int -> Bool
-castNotEqual = castEqual
+castNotEqual = undefined -- castEqual
 
 -- | Introduce single let binding, then cast there and back
 castSingleLet :: Int -> Int
@@ -64,3 +86,66 @@ castSingleLetAs x =
     case letAs x of
       LetAs (x' :: Identity t1) ->
         castEqual x'
+
+{-------------------------------------------------------------------------------
+  Small versions of the 'HList' tests that we use for size measurements
+
+  Since these work with only 3 indices, they are a bit more manageable for
+  easy experimentation.
+-------------------------------------------------------------------------------}
+
+hlistBaseline :: HList '[T "i00", T "i01", T "i02"]
+hlistBaseline =
+      -- 00 .. 09
+      HCons (MkT @"i00")
+    $ HCons (MkT @"i01")
+    $ HCons (MkT @"i02")
+    $ HNil
+
+hlistLetAs :: HList '[T "i00", T "i01", T "i02"]
+hlistLetAs =
+    case letAs (HCons (MkT @"i02") HNil) of { LetAs (xs02 :: HList t02) ->
+    case letAs (HCons (MkT @"i01") xs02) of { LetAs (xs01 :: HList t01) ->
+    case letAs (HCons (MkT @"i00") xs01) of { LetAs (xs00 :: HList t00) ->
+      castEqual xs00
+    }}}
+
+hlistLetAsCPS :: HList '[T "i00", T "i01", T "i02"]
+hlistLetAsCPS =
+    letT' (Proxy @'[T "i00", T "i01", T "i02"]) $ \(_ :: Proxy r) -> castEqual $
+      letAs' @(HList r) (HCons (MkT @"i02") HNil) $ \(xs02 :: HList t02) ->
+      letAs' @(HList r) (HCons (MkT @"i01") xs02) $ \(xs01 :: HList t01) ->
+      letAs' @(HList r) (HCons (MkT @"i00") xs01) $ \(xs00 :: HList t00) ->
+        castEqual xs00
+
+{-------------------------------------------------------------------------------
+  Similarly, small versions of the @<*>@ tests.
+-------------------------------------------------------------------------------}
+
+apBaseline :: Applicative f => (T "i00" -> T "i01" -> T "i02" -> r) -> f r
+apBaseline f =
+        pure f
+    <*> pure (MkT @"i00")
+    <*> pure (MkT @"i01")
+    <*> pure (MkT @"i02")
+
+apLet :: forall f r. Applicative f => (T "i00" -> T "i01" -> T "i02" -> r) -> f r
+apLet f =
+    case letT (Proxy @(T "i02" -> r))   of { LetT (_ :: Proxy l02) ->
+    case letT (Proxy @(T "i01" -> l02)) of { LetT (_ :: Proxy l01) ->
+    case letT (Proxy @(T "i00" -> l01)) of { LetT (_ :: Proxy l00) ->
+
+      let f00 :: f l00
+          f01 :: f l01
+          f02 :: f l02
+
+          res :: f r
+
+          f00 = pure (castEqual f)
+          f01 = castEqual f00 <*> pure (MkT @"i00")
+          f02 = castEqual f01 <*> pure (MkT @"i01")
+          res = castEqual f02 <*> pure (MkT @"i02")
+
+      in res
+
+    }}}
--- test/Test/Size/Setup.hs
+++ test/Test/Size/Setup.hs
@@ -7,8 +7,23 @@ import Data.Kind
 import GHC.TypeLits
 
 data HList :: [Type] -> Type where
-  HNil  :: HList '[]
-  HCons :: x -> HList xs -> HList (x : xs)
+    HNil  :: HList '[]
+    HCons :: x -> HList xs -> HList (x : xs)
 
 data T :: Symbol -> Type where
-  MkT :: T s
+    MkT :: T s
+  deriving (Show, Eq)
+
+{-------------------------------------------------------------------------------
+  Infrastructure for testing
+
+  These are standard definitions, and will definitely lead to quadratic core.
+-------------------------------------------------------------------------------}
+
+type family All (c :: k -> Constraint) (xs :: [k]) :: Constraint where
+  All c '[]       = ()
+  All c (x ': xs) = (c x, All c xs)
+
+deriving instance All Show xs => Show (HList xs)
+deriving instance All Eq   xs => Eq   (HList xs)
+
