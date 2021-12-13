4d6e61e5025e22066c25cfa52c88217713d7f2de
Edsko de Vries <edsko@well-typed.com>
2021-12-13 15:06:50 +0100

    Compatibility with 9.2

--- src/TypeLet/Plugin/GhcTcPluginAPI.hs
+++ src/TypeLet/Plugin/GhcTcPluginAPI.hs
@@ -24,30 +24,43 @@ module TypeLet.Plugin.GhcTcPluginAPI (
   , tyCoVarsOfType
     -- ** Location information
   , GenLocated(L)
+    -- ** Errors
+  , Panic.panic
   ) where
 
 import GHC.TcPlugin.API
 
 #if MIN_VERSION_ghc(9,0,0)
-
 import GHC.Core.TyCo.FVs (tyCoVarsOfType)
 import GHC.Core.TyCo.Subst (TCvSubst)
 import GHC.Tc.Utils.TcType (isSkolemTyVar)
 import GHC.Types.SrcLoc (GenLocated(L))
 import GHC.Types.Var (TyVar)
 import GHC.Types.Var.Set (elemVarSet)
-import GHC.Utils.Outputable
 
 import qualified GHC.Core.TyCo.Subst as Subst
+#endif
 
+#if MIN_VERSION_ghc(9,0,0)
+#if MIN_VERSION_ghc(9,2,0)
+import GHC.Utils.Outputable
+import qualified GHC.Utils.Panic as Panic
 #else
+import GHC.Utils.Outputable hiding (panic)
+import qualified GHC.Utils.Outputable as Panic
+#endif
+#endif
 
-import Outputable
+#if !MIN_VERSION_ghc(9,0,0)
+
+import Outputable hiding (panic)
 import SrcLoc (GenLocated(L))
 import Type (TyVar)
 import VarSet (elemVarSet)
-import TcType (TCvSubst)
+import TcType (TCvSubst, tyCoVarsOfType, isSkolemTyVar)
 
-import qualified TcType
+import qualified TcType     as Subst
+import qualified Outputable as Panic
 
 #endif
+
