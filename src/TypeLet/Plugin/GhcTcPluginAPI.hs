31af413ae012536f279c02dece38aaf9cb968f28
Edsko de Vries <edsko@well-typed.com>
2021-12-13 15:11:54 +0100

    Use GHC.Utils.Outputable from ghc-tcplugin-api

--- src/TypeLet/Plugin/GhcTcPluginAPI.hs
+++ src/TypeLet/Plugin/GhcTcPluginAPI.hs
@@ -4,12 +4,7 @@
 module TypeLet.Plugin.GhcTcPluginAPI (
     -- * Standard exports
     module GHC.TcPlugin.API
-
-#if MIN_VERSION_ghc(9,0,0)
   , module GHC.Utils.Outputable
-#else
-  , module Outputable
-#endif
 
     -- * Additional re-exports
     -- ** Type variables
@@ -41,26 +36,23 @@ import GHC.Types.Var.Set (elemVarSet)
 import qualified GHC.Core.TyCo.Subst as Subst
 #endif
 
-#if MIN_VERSION_ghc(9,0,0)
 #if MIN_VERSION_ghc(9,2,0)
 import GHC.Utils.Outputable
 import qualified GHC.Utils.Panic as Panic
 #else
+-- for ghc < 9.0, needs ghc-tcplugin-api's re-export of GHC.Utils.Outputable
 import GHC.Utils.Outputable hiding (panic)
 import qualified GHC.Utils.Outputable as Panic
-#endif
 #endif
 
 #if !MIN_VERSION_ghc(9,0,0)
 
-import Outputable hiding (panic)
 import SrcLoc (GenLocated(L))
 import Type (TyVar)
 import VarSet (elemVarSet)
 import TcType (TCvSubst, tyCoVarsOfType, isSkolemTyVar)
 
-import qualified TcType     as Subst
-import qualified Outputable as Panic
+import qualified TcType as Subst
 
 #endif
 
